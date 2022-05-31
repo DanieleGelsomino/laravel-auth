1ee6224873ce1342ad9b8c8930cffda2fc02230b
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-31 15:27:03 +0200

    create index post to fix

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>I Tuoi Post</h1>
+            </div>
+        </div>
+    </div>
+@endsection
